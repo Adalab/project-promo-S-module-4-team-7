facbc4a0bba700f81d671deb4bba2e78d44c13b4
AlmuLloret <almudenalloret@gmail.com>
2023-03-27 12:35:25 +0200

    copy project

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import "../styles/App.scss";
+import {Route, Routes} from 'react-router-dom';
+import Landing from "./Landing";
+import CreateProject from "./CreateProject"; 
+import Error404 from "./Error404";
+import ls from '../services/localStorage';
+import { useState } from "react";
+
+
+function App() {
+  const [allCards, setAllCards] = useState (ls.get('projectsLS', [])); 
+
+  const handleLs = (value) => {
+    setAllCards(value)
+  }
+
+  //Aqui la variable de estado del lS
+  // Lifting con el array de proyectos y la fx para modificar la variable de estado del lS 
+
+  return(
+    <Routes>
+      <Route path="/" element={<Landing allCards={allCards} />}>
+      <Route path="/*" element={<Error404 />}></Route>
+      </Route>
+      <Route path="/create" element={<CreateProject allCards={allCards} handleLs={handleLs} />}></Route>
+      <Route path="/create/*" element={<Error404 />}></Route>
+    </Routes>
+
+  );
+};
+
+export default App;
---
+++ src/components/Landing.js
@@ -0,0 +1,28 @@
+import "../styles/layout/Landing.scss";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Link } from "react-router-dom";
+import ProjectList from "./ProjectList";
+import Intro from "./Intro";
+
+function Landing({ allCards }) {
+  console.log(allCards);
+  return (
+    <>
+      <div
+        className={`container__landing ${
+          allCards.length >= 3 ? "min-height" : ""
+        }`}
+      >
+        <Header />
+        <main className="main__landing">
+          <Intro />
+          <ProjectList allCards={allCards} />
+        </main>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Landing;
