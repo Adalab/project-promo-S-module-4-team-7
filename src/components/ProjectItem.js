7e865bef3d88d4ef69fdc81c619e492b413af50e
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-03-27 18:29:51 +0200

    add link to every card

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "../styles/layout/ProjectItem.scss";
 // import ls from "../services/localStorage";
 
@@ -19,58 +20,60 @@ const ProjectItem = ({ eachCard, allCards, index }) => {
   // }
 
   return (
-    <li>
-      {/* <section className="pi__icons">
-                <a onClick={deleteCardTest}>
-                    <i className="fa-regular fa-circle-xmark"></i>
-                </a>
-                <a href={`${eachCard.urlD}`} target='_blank'>
-                    <i className="fa-solid fa-expand"></i>
-                </a>
-            </section> */}
-      <section className="author">
-        <section className="author__ip">
-          <p className="author__ip__subtitle">{eachCard.name}</p>
-          {/* <hr className='author__ip__line' /> */}
+    <Link>
+      <li>
+        {/* <section className="pi__icons">
+                  <a onClick={deleteCardTest}>
+                      <i className="fa-regular fa-circle-xmark"></i>
+                  </a>
+                  <a href={`${eachCard.urlD}`} target='_blank'>
+                      <i className="fa-solid fa-expand"></i>
+                  </a>
+              </section> */}
+        <section className="author">
+          <section className="author__ip">
+            <p className="author__ip__subtitle">{eachCard.name}</p>
+            {/* <hr className='author__ip__line' /> */}
 
-          <h2 className="author__ip__title">
-            {eachCard.name || "Elegant Workspace"}
-          </h2>
-          <p className="author__ip__slogan">
-            {eachCard.slogan || "Diseños Exclusivos"}
-          </p>
-          <p className="author__ip__desc">
-            {eachCard.desc ||
-              `Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit. Amet faucibus commodo
-                    tellus lectus lobortis.`}
-          </p>
-          <section className="author__ip__tech">
-            <p className="author__ip__tech__text">
-              {eachCard.technologies || "React JS, MongoDB"}
+            <h2 className="author__ip__title">
+              {eachCard.name || "Elegant Workspace"}
+            </h2>
+            <p className="author__ip__slogan">
+              {eachCard.slogan || "Diseños Exclusivos"}
             </p>
-            <div className="author__ip__tech__icons">
-              <a className="icon" href={`${eachCard.demo}`} target="_blank">
-                <i className="fa-solid fa-globe"></i>
-              </a>
-              <a href={`${eachCard.repo}`} target="_blank">
-                <i className="fa-brands fa-github"></i>
-              </a>
-            </div>
+            <p className="author__ip__desc">
+              {eachCard.desc ||
+                `Lorem ipsum dolor sit amet, consectetur
+                      adipiscing elit. Amet faucibus commodo
+                      tellus lectus lobortis.`}
+            </p>
+            <section className="author__ip__tech">
+              <p className="author__ip__tech__text">
+                {eachCard.technologies || "React JS, MongoDB"}
+              </p>
+              <div className="author__ip__tech__icons">
+                <a className="icon" href={`${eachCard.demo}`} target="_blank">
+                  <i className="fa-solid fa-globe"></i>
+                </a>
+                <a href={`${eachCard.repo}`} target="_blank">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </div>
+            </section>
+          </section>
+
+          <section className="author__ia">
+            <img className="author__ia__image" src={eachCard.photo} alt="" />
+            <p className="author__ia__job">
+              {eachCard.job || "Full Stack Developer"}
+            </p>
+            <p className="author__ia__name">
+              {eachCard.autor || "Emmelie Björklund"}
+            </p>
           </section>
         </section>
-
-        <section className="author__ia">
-          <img className="author__ia__image" src={eachCard.photo} alt="" />
-          <p className="author__ia__job">
-            {eachCard.job || "Full Stack Developer"}
-          </p>
-          <p className="author__ia__name">
-            {eachCard.autor || "Emmelie Björklund"}
-          </p>
-        </section>
-      </section>
-    </li>
+      </li>
+    </Link>
   );
 };
 
