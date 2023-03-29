8cbeb68fdd1e1e6cc9270d597e5a037373b7a97e
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-03-29 15:08:34 +0200

    add label to input

--- src/components/Form.js
+++ src/components/Form.js
@@ -19,163 +19,184 @@ function Form({
   const handleInputChange = (ev) => {
     handleInput(ev.target.value, ev.target.name);
   };
-
   const handleSubmit = (ev) => {
     ev.preventDefault();
   };
 
-  return (
-    <section className='form'>
-      <h2 className='form__title'>Información</h2>
-
-      <section className='form__ask-info'>
-        <p className='form__ask-info__subtitle'>Cuéntanos sobre el proyecto</p>
-        <hr className='form__ask-info__line' />
+return (
+    <section className="form">
+      <h2 className="form__title">Información</h2>
+      <section className="form__ask-info">
+        <p className="form__ask-info__subtitle">Cuéntanos sobre el proyecto</p>
+        <hr className="form__ask-info__line" />
       </section>
-      <form className='form__project' onSubmit={handleSubmit}>
-        <fieldset className='project'>
+      <form className="form__project" onSubmit={handleSubmit}>
+        <fieldset className="project">
+          <label htmlFor="name" className="project__label">
+            {" "}
+            Nombre del proyecto
+          </label>
           <input
             required
-            className='project__input'
-            type='text'
-            placeholder='Nombre del proyecto'
-            name='name'
-            id='name'
+            className="project__input"
+            type="text"
+            placeholder="Nombre del proyecto"
+            name="name"
+            id="name"
             minLength={2}
             value={data.name}
             onChange={handleInputChange}
           />
-          <p className='errorMessage'>{errorMessage.name}</p>
+          <p className="errorMessage">{errorMessage.name}</p>
+          <label htmlFor="slogan" className="project__label">
+            Slogan
+          </label>
           <input
-            className='project__input'
-            type='text'
-            name='slogan'
-            id='slogan'
-            placeholder='Slogan'
+            className="project__input"
+            type="text"
+            name="slogan"
+            id="slogan"
+            placeholder="Slogan"
             value={data.slogan}
             onChange={handleInputChange}
           />
-          <p className='errorMessage'>{errorMessage.slogan}</p>
+          <p className="errorMessage">{errorMessage.slogan}</p>
+          <label htmlFor="repo" className="project__label">
+            Repositorio
+          </label>
           <input
             required
-            className='project__input'
-            type='text'
-            name='repo'
-            id='repo'
-            placeholder='https://github.com/enlacedeturepo'
+            className="project__input"
+            type="text"
+            name="repo"
+            id="repo"
+            placeholder="https://github.com/enlacedeturepo"
             value={data.repo}
             onChange={handleInputChange}
           />
-          <p className='errorMessage'>{errorMessage.repo}</p>
+          <p className="errorMessage">{errorMessage.repo}</p>
+          <label htmlFor="demo" className="project__label">
+            Demo
+          </label>
           <input
             required
-            className='project__input'
-            type='text'
-            placeholder=' https://enlacedetuproyecto'
-            name='demo'
-            id='demo'
+            className="project__input"
+            type="text"
+            placeholder=" https://enlacedetuproyecto"
+            name="demo"
+            id="demo"
             value={data.demo}
             onChange={handleInputChange}
           />
-          <p className='errorMessage'>{errorMessage.demo}</p>
+          <p className="errorMessage">{errorMessage.demo}</p>
+          <label htmlFor="technologies" className="project__label">
+            Tecnologías
+          </label>
           <input
             required
-            className='project__input'
-            type='text'
-            placeholder='Tecnologías'
-            name='technologies'
-            id='technologies'
+            className="project__input"
+            type="text"
+            placeholder="Tecnologías"
+            name="technologies"
+            id="technologies"
             value={data.technologies}
             onChange={handleInputChange}
           />
-          <p className='errorMessage'>{errorMessage.technologies}</p>
+          <p className="errorMessage">{errorMessage.technologies}</p>
+          <label htmlFor="desc" className="project__label">
+            Descripción del proyecto
+          </label>
           <textarea
-            className='project__textarea'
-            type='text'
-            placeholder='Descripción'
-            name='desc'
-            id='desc'
+            className="project__textarea"
+            type="text"
+            placeholder="Descripción"
+            name="desc"
+            id="desc"
             value={data.desc}
-            onChange={handleInputChange}></textarea>
-          <p className='errorMessage'>{errorMessage.desc}</p>
+            onChange={handleInputChange}
+          ></textarea>
+          <p className="errorMessage">{errorMessage.desc}</p>
         </fieldset>
-
-        <section className='ask-info__autor'>
-          <p className='ask-info__autor__subtitle'>Cuéntanos sobre la autora</p>
-          <hr className='ask-info__autor__line' />
+        <section className="ask-info__autor">
+          <p className="ask-info__autor__subtitle">Cuéntanos sobre la autora</p>
+          <hr className="ask-info__autor__line" />
         </section>
-
-        <fieldset className='autor'>
+        <fieldset className="autor">
+          <label htmlFor="autor" className="project__label">
+            Nombre autora del proyecto
+          </label>
           <input
             required
-            className='autor__input'
-            type='text'
-            placeholder='Nombre'
-            name='autor'
-            id='autor'
+            className="autor__input"
+            type="text"
+            placeholder="Nombre"
+            name="autor"
+            id="autor"
             value={data.autor}
             onChange={handleInputChange}
           />
-          <p className='errorMessage'>{errorMessage.autor}</p>
+          <p className="errorMessage">{errorMessage.autor}</p>
+          <label htmlFor="job" className="project__label">
+            Trabajo{" "}
+          </label>
           <input
-            className='autor__input'
-            type='text'
-            placeholder='Trabajo'
-            name='job'
-            id='job'
+            className="autor__input"
+            type="text"
+            placeholder="Trabajo"
+            name="job"
+            id="job"
             value={data.job}
             onChange={handleInputChange}
           />
-          <p className='errorMessage'>{errorMessage.job}</p>
+          <p className="errorMessage">{errorMessage.job}</p>
         </fieldset>
-
-        <section className='buttons-images'>
+        <section className="buttons-images">
           <GetAvatar
-            className='buttons-img__btn'
-            text='Subir foto de proyecto'
+            className="buttons-img__btn"
+            text="Subir foto de proyecto"
             updateAvatar={updatePhoto}
             handleClickCreateCard={handleClickCreateCard}
           />
-
           <GetAvatar
-            className='buttons-img__btn'
-            text='Subir foto de autora'
+            className="buttons-img__btn"
+            text="Subir foto de autora"
             updateAvatar={updateImages}
             handleClickCreateCard={handleClickCreateCard}
           />
         </section>
-        <section className='buttons-img-2'>
+        <section className="buttons-img-2">
           <Button
-            className='buttons-img-2__btn-large'
-            text='Crear Tarjeta'
+            className="buttons-img-2__btn-large"
+            text="Crear Tarjeta"
             handleClickCreateCard={handleClickCreateCard}
           />
           <button
-            title='Haz click aquí para empezar desde cero'
+            title="Haz click aquí para empezar desde cero"
             onClick={handleResetButton}
-            className='button-reset'>
+            className="button-reset"
+          >
             {/* <i class='fa fa-refresh'></i> */}
-            <i class='fa-regular fa-trash-can'></i>
+            <i class="fa-regular fa-trash-can"></i>
           </button>
-
-          <p className='errorMessage'>{errorMessage.photo}</p>
-          <p className='errorMessage'>{errorMessage.image}</p>
+          <p className="errorMessage">{errorMessage.photo}</p>
+          <p className="errorMessage">{errorMessage.image}</p>
         </section>
-        <section className='card'>
-          <span className=''></span>
+        <section className="card">
+          <span className=""></span>
           <a
             href={`${url}`}
             className={`card__msg ${classHidden ? "hidden" : true}`}
-            target='_blank'
-            rel='noreferrer'
-            onSubmit={handleSubmit}>
+            target="_blank"
+            rel="noreferrer"
+            onSubmit={handleSubmit}
+          >
             {cardMessage}
           </a>
-          <p className='errorMessage'>{errorMessageCard}</p>
+          <p className="errorMessage">{errorMessageCard}</p>
         </section>
       </form>
     </section>
   );
-}
+};
 
 export default Form;
