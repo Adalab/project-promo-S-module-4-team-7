f2d46e1df063c0367be5af2a3108917c5dc2b3f8
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-03-30 13:41:57 +0200

    review styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import logoAdalab from "../images/logo-adalab.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
 import "../styles/layout/Header.scss";
+import logoGrupo7 from "../images/grupo7.svg"
 
 const element = <FontAwesomeIcon icon={faLaptopCode} />;
 
@@ -10,10 +11,7 @@ function Header() {
   return (
     <header className='header'>
       <div className='icon'>
-        <a href='#' className='header__icon'>
-          {element}
-        </a>
-        <p className='text'>Proyectos Molones</p>
+        <img src={logoGrupo7} className="header__icon"></img>
       </div>
       <a href="https://adalab.es/" target="_blank" className="footer__logo">
         <img className='logo-adalab' src={logoAdalab} alt='logo Adalab' />
