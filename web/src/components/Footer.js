607ed8882b28c235013dcce84f69634f15f5e1fe
AlmuLloret <almudenalloret@gmail.com>
2023-04-02 22:00:56 +0200

    Add link to detail project on footer logo

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -5,19 +5,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
 import "../styles/layout/Footer.scss";
 
-
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <Link to="/">
-                <img src={logoGrupo7} className="header__icon">
-                </img>
-            </Link>
-            <a href="https://adalab.es/" target="_blank" className="footer__logo">
-                <img className='footer__logo__logo-adalab' src={logoAdalab} />
-            </a>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <Link to="/detailcard">
+        <img src={logoGrupo7} className="header__icon"></img>
+      </Link>
+      <a href="https://adalab.es/" target="_blank" className="footer__logo">
+        <img className="footer__logo__logo-adalab" src={logoAdalab} />
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
