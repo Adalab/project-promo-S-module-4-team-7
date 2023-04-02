957bcba4965f2ad238f1b4f9edbdf4c86fec97cc
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-04-02 11:17:19 +0200

    add logo to footer

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -1,16 +1,18 @@
+import { Link } from "react-router-dom";
 import logoAdalab from "../images/logo-adalab.png";
+import logoGrupo7 from "../images/grupo7.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
 import "../styles/layout/Footer.scss";
-const element = <FontAwesomeIcon icon={faLaptopCode} />;
 
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <a href="#" className='footer__icon'>
-                {element}
-            </a>
+            <Link to="/">
+                <img src={logoGrupo7} className="header__icon">
+                </img>
+            </Link>
             <a href="https://adalab.es/" target="_blank" className="footer__logo">
                 <img className='footer__logo__logo-adalab' src={logoAdalab} />
             </a>
--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -3,7 +3,8 @@ import logoAdalab from "../images/logo-adalab.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
 import "../styles/layout/Header.scss";
-import logoGrupo7 from "../images/grupo7.svg"
+import logoGrupo7 from "../images/grupo7.svg";
+import { Link } from "react-router-dom";
 
 const element = <FontAwesomeIcon icon={faLaptopCode} />;
 
@@ -11,7 +12,10 @@ function Header() {
   return (
     <header className='header'>
       <div className='icon'>
-        <img src={logoGrupo7} className="header__icon"></img>
+        <Link to="/">
+          <img src={logoGrupo7} className="header__icon">
+          </img>
+        </Link>
       </div>
       <a href="https://adalab.es/" target="_blank" className="footer__logo">
         <img className='logo-adalab' src={logoAdalab} alt='logo Adalab' />
