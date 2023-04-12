3c37284b4453446af8bfc8e98d015f652dbe5e1f
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-04-12 11:55:55 +0200

    change port

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.set("view engine", "ejs");
 
-const port = 4000;
+const serverPort = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
