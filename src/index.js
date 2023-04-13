0a8e5a91e393875d7857c788892ba1780eb8257c
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-04-13 22:19:10 +0200

    refactoring backend code

--- src/index.js
+++ src/index.js
@@ -19,54 +19,101 @@ const swaggerFile = require("./swagger.json");
 //Especificar en el server use
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
+async function api() {
+  const resp = await fetch('');
+  const data = await resp.json();
+  console.log(data);
+}
+
+
 let connection;
 
-mysql
-  .createConnection({
-    host: "sql.freedb.tech",
-    database: "freedb_grupo7",
-    user: "freedb_root_grupo7",
-    password: "KWuEnYU9W2Nc%5a",
-    idleTimeout: 1000000,
-  })
-  .then((conn) => {
-    connection = conn;
-    connection
-      .connect()
-      .then(() => {
-        console.log(
-          `Conexión establecida con la base de datos (identificador=${connection.threadId})`
-        );
-      })
-      .catch((err) => {
-        console.error("Error de conexion: " + err.stack);
-      });
-  })
-  .catch((err) => {
-    console.error("Error de configuración: " + err.stack);
+async function getConnection() {
+  const connection = await mysql.createConnection({
+    host: 'sql.freedb.tech',
+    database: 'freedb_Full Of Projects',
+    user: 'freedb_Base64',
+    password: 'HFpUad@JqahhC6d',
   });
+  await connection.connect();
 
-app.get("/api/projects/all", (req, res) => {
-  let sql =
-    "SELECT * FROM projects,authors WHERE projects.fkIdAuthor = authors.idauthor";
-  connection
-    .query(sql)
-    .then(([results, fields]) => {
-      console.log("Información recuperada:");
-      results.forEach((result) => {
-        console.log(result);
-      });
+  console.log(
+    `Conexión establecida con la base de datos (identificador=${connection.threadId})`
+  );
 
-      res.json(results);
+  return connection;
+  /*
+  return mysql
+    .createConnection({
+      host: 'sql.freedb.tech',
+      database: 'freedb_Full Of Projects',
+      user: 'freedb_Base64',
+      password: 'HFpUad@JqahhC6d',
+    })
+    .then((connection) => connection.connect())
+    .then((connection) => {
+      console.log(
+        `Conexión establecida con la base de datos (identificador=${connection.threadId})`
+      );
+      return connection;
     })
     .catch((err) => {
-      throw err;
+      console.error('Error de configuración: ' + err.stack);
     });
+    */
+}
+
+// mysql
+//   .createConnection({
+//     host: "sql.freedb.tech",
+//     database: "freedb_grupo7",
+//     user: "freedb_root_grupo7",
+//     password: "KWuEnYU9W2Nc%5a",
+//     idleTimeout: 1000000,
+//   })
+//   .then((conn) => {
+//     connection = conn;
+//     connection
+//       .connect()
+//       .then(() => {
+//         console.log(
+//           `Conexión establecida con la base de datos (identificador=${connection.threadId})`
+//         );
+//       })
+//       .catch((err) => {
+//         console.error("Error de conexion: " + err.stack);
+//       });
+//   })
+//   .catch((err) => {
+//     console.error("Error de configuración: " + err.stack);
+//   });
+
+app.get("/api/projects/all", async (req, res) => {
+  let sql =
+    "SELECT * FROM projects,authors WHERE projects.fkIdAuthor = authors.idauthor";
+
+  const connection = await getConnection();
+  const [results, fields] = await connection.query(sql);
+  res.json(results);
+  connection.end();
+
+  // connection
+  //   .query(sql)
+  //   .then(([results, fields]) => {
+  //     console.log("Información recuperada:");
+  //     results.forEach((result) => {
+  //       console.log(result);
+  //     });
+
+  //     res.json(results);
+  //   })
+  //   .catch((err) => {
+  //     throw err;
+  //   });
 });
 
-app.post("/api/projects/add", (req, res) => {
+app.post("/api/projects/add", async (req, res) => {
   const data = req.body;
-  console.log(data);
   if (
     data.name &&
     data.desc &&
@@ -81,6 +128,40 @@ app.post("/api/projects/add", (req, res) => {
   ) {
     let sqlAuthor = "INSERT INTO authors (autor, job, photo) VALUES (?, ?, ?)";
     let valuesAuthor = [data.autor, data.job, data.photo];
+
+    const connection = await getConnection();
+    const [results, fields] = await connection.query(sqlAuthor, valuesAuthor);
+    console.log(results);
+
+      let sqlProjects =
+        "INSERT INTO projects (name, slogan, technologies, demo, repo, `desc`, image, fkIdAuthor) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+      let valuesProject = [
+        data.name,
+        data.desc,
+        data.slogan,
+        data.repo,
+        data.demo,
+        data.technologies,
+        data.image,
+        results.insertId,
+      ];
+
+      const [resultsInsert] = await connection.query(sqlProjects, valuesProject);
+      let response = {
+          success: true,
+          cardURL: `http://localhost:4000/api/projects/detail/${resultsInsert.insertId}`,
+        };
+        console.log(results);
+        res.json(response);
+    } else {
+      let responseFalse = { success: false };
+      res.json(responseFalse);
+    }
+    /**
+     * refactoring from here
+     */
+    /*
     connection.query(sqlAuthor, valuesAuthor).then(([results, fields]) => {
       console.log(results);
 
@@ -112,36 +193,31 @@ app.post("/api/projects/add", (req, res) => {
     };
     res.json(responseFalse);
   }
+  */
 });
 
-app.get("/api/projects/detail/:projectID", (req, res) => {
+app.get("/api/projects/detail/:projectID", async (req, res) => {
   const projectID = req.params.projectID;
   const sql =
     "SELECT * FROM projects, authors WHERE projects.fkIdAuthor = authors.idauthor AND idprojects = ?";
 
-  connection
-    .query(sql, [projectID])
-    .then(([results, fields]) => {
+  const connection = await getConnection();
+    const [results, fields] = await connection.query(sql, [projectID])
       res.render("project_detail", results[0]);
-    })
-    .catch((err) => {
-      throw err;
-    });
+     connection.end();
 });
 
+
 // delete project
-app.delete("/api/projects/delete/:projectID", (req, res) => {
+app.delete("/api/projects/delete/:projectID", async (req, res) => {
   const projectID = req.params.projectID;
   let sql = "DELETE FROM projects WHERE idprojects = ?; ";
-  connection
-    .query(sql, [projectID])
-    .then(([results, fields]) => {
-      console.log("Proyecto eliminado");
-      res.json(results);
-    })
-    .catch((err) => {
-      throw err;
-    });
+  const connection = await getConnection();
+  const [results] = await connection.query(sql, [projectID])
+
+  console.log("Proyecto eliminado");
+  res.json(results);
+  connection.end();
 });
 
 app.use(express.static("./src/public-react"));
